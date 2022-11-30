4f78ecabb61cbb550ec9627805e50ba13eb25c49
Robert Manders <31056731+Rob-Manders@users.noreply.github.com>
2022-11-30 20:47:21 +0000

    Clean up a bit.

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,13 @@ import { calculateRate } from './modules/calculate-rate'
 
 export function calculateTax(propertyValue: number): number {
   const { rates, highestRate, minPropertyValue }: ResidentialRates = fetchRates()
-  const topValue = rates[rates.length - 1][0]
+  const highestValue = rates[rates.length - 1][0]
 
   if (propertyValue <= minPropertyValue) return 0
 
-  let tax: number = propertyValue > topValue ? (propertyValue - topValue) * highestRate : 0
+  let tax: number = propertyValue > highestValue ? (propertyValue - highestValue) * highestRate : 0
 
   let index: number = 0
-
   while (index === 0 || (rates[index] && propertyValue > rates[index - 1][0])) {
     tax += calculateRate(propertyValue, minPropertyValue, rates[index], rates[index - 1], index)
     index++
--- src/modules/calculate-rate.ts
+++ src/modules/calculate-rate.ts
@@ -7,11 +7,15 @@ export function calculateRate(
 ): number {
   const [bandValue, rate] = currentRate
   const previousBandValue = index > 0 ? previousRate[0] : 0
+
   let value: number
 
   if (propertyValue > previousBandValue) {
-    if (propertyValue < bandValue) value = propertyValue - previousBandValue
-    else value = bandValue - previousBandValue
+    if (propertyValue < bandValue) {
+      value = propertyValue - previousBandValue
+    } else {
+      value = bandValue - previousBandValue
+    }
   } else {
     return 0
   }
